9ae000d94601750f3ef1bbc9208cb5d7960afac9
Thunnini <yunjh1994@gmail.com>
2026-05-12 19:55:16 +0900

    Improve duplicate import error messages

--- lib/src/features/onboarding/shared/onboarding_error_messages.dart
+++ lib/src/features/onboarding/shared/onboarding_error_messages.dart
@@ -1,14 +1,33 @@
 import '../../../providers/account_provider.dart';
 
+const kDuplicateSecretPassphraseImportErrorMessage =
+    'This account is already in your wallet.';
+const kDuplicateKeystoneAccountImportErrorMessage =
+    'This Keystone account is already in your wallet.';
+const kDuplicateAccountImportErrorMessage =
+    'This account is already in your wallet.';
+
 String onboardingSubmitErrorMessage(Object error) {
   if (error is WalletCreationCurrentBlockHeightException) {
     return kWalletCreationCurrentBlockHeightErrorMessage;
   }
 
   const exceptionPrefix = 'Exception: ';
-  final message = error.toString();
+  var message = error.toString();
   if (message.startsWith(exceptionPrefix)) {
-    return message.substring(exceptionPrefix.length);
+    message = message.substring(exceptionPrefix.length);
+  }
+  final anyhowMatch = RegExp(r'^AnyhowException\((.*)\)$').firstMatch(message);
+  if (anyhowMatch != null) {
+    message = anyhowMatch.group(1)!;
+  }
+  if (_isAccountCollisionMessage(message)) {
+    return kDuplicateAccountImportErrorMessage;
   }
   return message;
+}
+
+bool _isAccountCollisionMessage(String message) {
+  return message.contains('account corresponding to the data provided') &&
+      message.contains('already exists in the wallet');
 }
