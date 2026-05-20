8df8d0d9c7523f79137fc306b7e2a9c3be4d28a6
Julian Abraham <julian.abraham@xaviers.edu.in>
2026-05-20 22:36:07 +0530

    feat(zns): add ZnsResolver provider — ZNS name resolution infrastructure

---
+++ lib/src/providers/zns_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zcashname_sdk/zcashname_sdk.dart';
+
+import 'rpc_endpoint_provider.dart';
+
+class ZnsResolver {
+  ZnsResolver(this._zns);
+
+  final ZNS _zns;
+
+  Future<String?> resolveName(String name) async {
+    final registration = await _zns.resolveName(name);
+    return registration?.address;
+  }
+
+  void close() => _zns.close();
+}
+
+final znsResolverProvider = Provider<ZnsResolver>((ref) {
+  final networkName = ref.watch(
+    rpcEndpointProvider.select((config) => config.networkName),
+  );
+
+  final zns = switch (networkName) {
+    'main' => ZNS(network: Network.mainnet),
+    'test' => ZNS(network: Network.testnet),
+    _ => ZNS(url: Uri.parse('http://localhost:3000')),
+  };
+
+  final resolver = ZnsResolver(zns);
+  ref.onDispose(resolver.close);
+  return resolver;
+});
