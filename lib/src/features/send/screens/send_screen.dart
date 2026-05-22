c11e2ebeaa3dc65e2eb3b1a3834d61db998bc63f
rowan <crewe1746@naver.com>
2026-05-22 15:29:42 +0900

    Keep payment URI prefill during send route refresh

--- lib/src/features/send/screens/send_screen.dart
+++ lib/src/features/send/screens/send_screen.dart
@@ -45,8 +45,26 @@ class SendScreen extends ConsumerStatefulWidget {
 }
 
 class _SendScreenState extends ConsumerState<SendScreen> {
+  SendPrefillArgs? _retainedPrefill;
+
+  @override
+  void initState() {
+    super.initState();
+    _retainedPrefill = widget.prefill;
+  }
+
+  @override
+  void didUpdateWidget(covariant SendScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    final prefill = widget.prefill;
+    if (prefill != null) {
+      _retainedPrefill = prefill;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final prefill = widget.prefill ?? _retainedPrefill;
     final walletAsync = ref.watch(walletProvider);
     final accountState = ref.watch(accountProvider).value;
     final activeAccountUuid = accountState?.activeAccountUuid;
@@ -61,12 +79,12 @@ class _SendScreenState extends ConsumerState<SendScreen> {
     final spendableBalance = sync.spendableBalance;
 
     return _SendComposeBody(
-      key: ValueKey('$activeAccountUuid:${widget.prefill?.fingerprint ?? ''}'),
+      key: ValueKey('$activeAccountUuid:${prefill?.fingerprint ?? ''}'),
       walletAsync: walletAsync,
       activeAccountUuid: activeAccountUuid,
       activeAccountIsHardware: activeAccountIsHardware,
       spendableBalance: spendableBalance,
-      prefill: widget.prefill,
+      prefill: prefill,
     );
   }
 }
