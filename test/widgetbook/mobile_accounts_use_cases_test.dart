6707ee39b6a6d5f94fa2baf875c9266fcee81a6f
rowan <crewe1746@naver.com>
2026-08-03 16:33:01 +0900

    [mobile] Run account menu previews in mobile lane

---
+++ test/widgetbook/mobile_accounts_use_cases_test.dart
@@ -0,0 +1,73 @@
+@Tags(['mobile'])
+library;
+
+import 'package:flutter/material.dart' show MaterialApp;
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zcash_wallet/src/core/theme/app_theme.dart';
+import 'package:zcash_wallet/widgetbook/screen_use_cases.dart';
+
+void main() {
+  testWidgets('mobile accounts menu hides the shortcut for Keystone', (
+    tester,
+  ) async {
+    await _pumpMobileAccountsUseCase(tester, buildMobileAccountsUseCase);
+
+    await tester.tap(
+      find.byKey(const ValueKey('mobile_accounts_menu_preview-account-2')),
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.text('View secret passphrase'), findsNothing);
+
+    await tester.tapAt(const Offset(10, 10));
+    await tester.pumpAndSettle();
+    await tester.tap(
+      find.byKey(const ValueKey('mobile_accounts_menu_preview-account-3')),
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.text('View secret passphrase'), findsOneWidget);
+    expect(tester.takeException(), isNull);
+  });
+
+  testWidgets('mobile account menu use cases distinguish software and '
+      'Keystone accounts', (tester) async {
+    await _pumpMobileAccountsUseCase(
+      tester,
+      buildMobileAccountsSoftwareMenuUseCase,
+    );
+
+    expect(find.text('View secret passphrase'), findsOneWidget);
+    expect(tester.takeException(), isNull);
+
+    await _pumpMobileAccountsUseCase(
+      tester,
+      buildMobileAccountsKeystoneMenuUseCase,
+    );
+
+    expect(find.text('View secret passphrase'), findsNothing);
+    expect(find.text('Copy address'), findsOneWidget);
+    expect(tester.takeException(), isNull);
+  });
+}
+
+Future<void> _pumpMobileAccountsUseCase(
+  WidgetTester tester,
+  WidgetBuilder builder,
+) async {
+  tester.view.physicalSize = const Size(393, 852);
+  tester.view.devicePixelRatio = 1.0;
+  addTearDown(tester.view.resetPhysicalSize);
+  addTearDown(tester.view.resetDevicePixelRatio);
+
+  await tester.pumpWidget(
+    MaterialApp(
+      key: UniqueKey(),
+      builder: (context, child) =>
+          AppTheme(data: AppThemeData.light, child: child!),
+      home: Builder(builder: builder),
+    ),
+  );
+  await tester.pumpAndSettle();
+}
