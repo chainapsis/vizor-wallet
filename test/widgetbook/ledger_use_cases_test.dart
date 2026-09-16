a39f4a5efa779aa2a6aa366773852ca785bee731
Thunnini <yunjh1994@gmail.com>
2026-09-16 22:18:18 +0900

    test(ledger): select reconnect guidance in readiness preview

--- test/widgetbook/ledger_use_cases_test.dart
+++ test/widgetbook/ledger_use_cases_test.dart
@@ -58,6 +58,7 @@ void main() {
       (_) => buildLedgerSigningPreview(
         phase: LedgerSigningModalPhase.failed,
         readiness: LedgerSigningPlaygroundReadiness.failed,
+        failureMode: LedgerSigningPlaygroundFailure.reconnect,
       ),
     );
 
